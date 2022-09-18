706976c8e12f21f77afe4880bd409c2137d2b24b
Nadeem-png <nk27890@gmail.com>
2022-09-17 21:55:26 -0700

    first commit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,51 @@
 import React from 'react'
 
-import img1 from'../asset/images/img3.jpg'
-import img2 from'../asset/images/img5.jpg'
 
-import img3 from'../asset/images/img6.jpg'
 
 
 export default function Header() {
   return (
   
     <div className='bg'>
-     <div className='container '>
-      <img src=''/>
+     <div className='container-fluid '>
+     <nav class="navbar navbar-expand-lg ">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
+      <a class="navbar-brand font-weight-bold" href="#">TravelYarri</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto ">
+        <li class="nav-item ">
+          <a class="nav-link active font-weight-bold" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Service</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Team</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link font-weight-bold " href="#" tabindex="-1" aria-disabled="true">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item ms-5">
+          <a class="nav-link" href="#">SignIn</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">SignOut</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
 
      </div>
 
